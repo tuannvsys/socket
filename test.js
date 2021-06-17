0d3eba1237afc7901ca787487c6fbfafc667566e
tuannvsys <tuannv@saisystem.vn>
2021-06-17 14:45:17 +0700

    add

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var io = require('socket.io-client');
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 1000; i++) {
     try {
         // var socket = io.connect("http://192.168.10.36", {  transports: ['websocket'] }); // transports: ['polling']
         var socket = io.connect("http://127.0.0.1", { transports: ['websocket'] }); // transports: ['polling']
