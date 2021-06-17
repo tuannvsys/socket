0e975441a345c0815fcdfdf336e6eafb82e78590
tuannvsys <tuannv@saisystem.vn>
2021-06-17 13:32:58 +0700

    add Log PORT

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             script: "./index.js",
             watch: true,
             env: {
-                PORT: 3000
+                PORT: 3001
             }
         },
         {
@@ -13,15 +13,55 @@ module.exports = {
             script: "./index.js",
             watch: true,
             env: {
-                PORT: 4000
+                PORT: 3002
             }
         },
-         {
-            name: "App2",
+        {
+            name: "App3",
             script: "./index.js",
             watch: true,
             env: {
-                PORT: 5000
+                PORT: 3003
+            }
+        },
+        {
+            name: "App4",
+            script: "./index.js",
+            watch: true,
+            env: {
+                PORT: 3004
+            }
+        },
+        {
+            name: "App5",
+            script: "./index.js",
+            watch: true,
+            env: {
+                PORT: 3005
+            }
+        },
+        {
+            name: "App6",
+            script: "./index.js",
+            watch: true,
+            env: {
+                PORT: 3006
+            }
+        }
+        ,{
+            name: "App7",
+            script: "./index.js",
+            watch: true,
+            env: {
+                PORT: 3007
+            }
+        },
+        {
+            name: "App8",
+            script: "./index.js",
+            watch: true,
+            env: {
+                PORT: 3008
             }
         }
     ]
