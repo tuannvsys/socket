afc85fbfe82043dcfafc65e56c847ba5a5eea622
tuannvsys <tuannv@saisystem.vn>
2021-06-18 09:10:50 +0700

    Add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,14 +8,14 @@ module.exports = {
                 PORT: 3001
             }
         },
-        // {
-        //     name: "App2",
-        //     script: "./index.js",
-        //     watch: true,
-        //     env: {
-        //         PORT: 3002
-        //     }
-        // },
+        {
+            name: "App2",
+            script: "./index.js",
+            watch: true,
+            env: {
+                PORT: 3002
+            }
+        },
         // {
         //     name: "App3",
         //     script: "./index.js",
