16e44326773110f6eeb68c5cd40f82fa07c57fc8
tuannvsys <tuannv@saisystem.vn>
2021-06-18 09:17:03 +0700

    Add

--- testLocal.js
+++ testLocal.js
@@ -1,6 +1,6 @@
 var io = require('socket.io-client');
 
-var socket = io.connect("http://45.32.60.107:3001", { transports: ['websocket'] })
+var socket = io.connect("http://127.0.0.1:3001", { transports: ['websocket'] })
 socket.on("hello", (data) => {
     console.log(">>>>>>>>>>:", data)
 });
--- testTwo.js
+++ testTwo.js
@@ -1,6 +1,6 @@
 var io = require('socket.io-client');
 
-var socket = io.connect("http://45.32.60.107:3001", { transports: ['websocket'] })
+var socket = io.connect("http://45.32.60.107:3002", { transports: ['websocket'] })
 socket.on("hello", (data) => {
     console.log(">>>>>>>>>>:", data)
 });
